6f57d93776d66287b4044799f144afde2c171f90
JiHun <74837762+ji-hun-choi@users.noreply.github.com>
2022-03-20 21:36:15 +0900

    Delete 인출력_사칙연산 directory

--- 인출력_사칙연산/02588.py
+++
@@ -1,7 +0,0 @@
-import sys
-num1 = sys.stdin.readline()
-num2 = sys.stdin.readline()
-print(int(num1)*int(num2[2]))
-print(int(num1)*int(num2[1]))
-print(int(num1)*int(num2[0]))
-print(int(num1)*int(num2))
--- 인출력_사칙연산/10869.py
+++
@@ -1,4 +0,0 @@
-a, b = map(int, input().split(' '))
-controll = ['+','-','*','/','%']
-for i in controll:
-    print(a+int(i)+b)
