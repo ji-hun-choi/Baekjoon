9b1a7ceb1c935798a15f322176b25da33649aad8
JiHun <74837762+ji-hun-choi@users.noreply.github.com>
2022-03-20 21:36:41 +0900

    Add files via upload

---
+++ 01.인출력_사칙연산/02588.py
@@ -0,0 +1,7 @@
+import sys
+num1 = sys.stdin.readline()
+num2 = sys.stdin.readline()
+print(int(num1)*int(num2[2]))
+print(int(num1)*int(num2[1]))
+print(int(num1)*int(num2[0]))
+print(int(num1)*int(num2))
