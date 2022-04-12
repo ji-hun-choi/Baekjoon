ac91e1eabfca6d186d4124aa791da52958ef0aad
최지훈 <chwlgns620@naver.com>
2022-04-12 10:06:24 +0900

    Add file
    8장 동기화

---
+++ 08.기본_수학2/01978.py
@@ -0,0 +1,14 @@
+n = int(input())
+nums = list(map(int, input().split(' ')))
+res = 0
+
+for i in nums:
+    cnt = 0
+    if i == 1 :
+        continue
+    for j in range(2, i+1):
+        if(i % j == 0):
+            cnt+=1
+    if(cnt == 1):
+        res += 1
+print(res)
